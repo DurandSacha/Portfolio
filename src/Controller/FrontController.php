6f936fc9d501b82c829ebc00bf8f4460303fcdbf
DurandSacha <sacha6623@gmail.Com>
2019-10-14 22:42:34 +0200

    symfony-structure

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('home.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('contact.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/laboratory", name="laboratory")
+     */
+    public function laboratory()
+    {
+        return $this->render('laboratory.html.twig', [
+        ]);
+    }
+}
