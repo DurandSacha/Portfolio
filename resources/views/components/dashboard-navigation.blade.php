78bef512e27e93d950c6080e742bb747dc9b02da
sdurand <sacha.durand@studio-net.fr>
2023-01-31 23:46:07 +0100

    feat(design): beginning navbar design in admin

---
+++ resources/views/components/dashboard-navigation.blade.php
@@ -0,0 +1,7 @@
+<div class="h-screen w-300 bg-gray-800 text-white left-0 top-0 -translate-x-0">
+    <nav class="px-4 py-3 h-full">
+      <a href="#" class="block font-medium py-2 hover:bg-gray-700">Mailing</a>
+      <a href="#" class="block font-medium py-2 hover:bg-gray-700">Stats</a>
+      <a href="#" class="block font-medium py-2 hover:bg-gray-700"></a>
+    </nav>
+  </div>
--- resources/views/dashboard/welcome.blade.php
+++ resources/views/dashboard/welcome.blade.php
@@ -1,1 +1,9 @@
-<p>Welcome to admin dashboard</p>
+@extends('layout')
+<div class="flex flex-row">
+    <div class="w-1/6">
+        <x-dashboard-navigation/>
+    </div>
+    <div class="p-8">
+        <p>Welcome to admin dashboard</p>
+    </div>
+</div>
