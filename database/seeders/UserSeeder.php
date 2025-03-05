0d078ab262750cddd24acd0a626ce5fb27c9a7f3
Sacha Durand <sacha.durand@studio-net.fr>
2025-03-05 19:27:20 +0100

    try to update seed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::firstOrNew([
+            'email' => 'sacha6623@gmail.com',
+        ]);
+
+        if (!$user->exists) {
+            $user->name = 'Sacha Durand';
+            $user->email = 'sacha6623@gmail.com';
+            $user->password = Hash::make(getenv('ADMIN_PASS'));
+            $user->roles = 'admin';
+            $user->save();
+        }
+    }
+}
